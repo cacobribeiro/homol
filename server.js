3021acdb8f9776769eeef6a14275640f6c8efb9f
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-04-01 10:24:09 -0300

    fix

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-
-
 // Parser de request bodies: json e x-www-form-urlencoded.
-app.use(bodyParser.json({limit: '100mb'}));
-app.use(bodyParser.urlencoded({limit: '100mb', extended: true }));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 // Middleware que adiciona um UUID a cada requisição. Serve para criar
 // threads de auditoria nos logs.
-
 
 const port = process.env.PORT || 3000;
 
