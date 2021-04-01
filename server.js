84774ec329d70e68d9fed62d87ff3e1ee6a90e57
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-04-01 10:21:19 -0300

    commit to HOMOL

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+
+
+// Parser de request bodies: json e x-www-form-urlencoded.
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true }));
+
+// Middleware que adiciona um UUID a cada requisição. Serve para criar
+// threads de auditoria nos logs.
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => console.log(`Server is running on port ${port}.`));
